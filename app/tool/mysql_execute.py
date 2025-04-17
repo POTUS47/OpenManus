2532ccdc1c52dc5276447e95117f984882c4cca5
sleep-sleep-yyy <1051095699@qq.com>
2025-04-17 22:39:02 +0800

    添加描述，并设置传参默认值

--- app/tool/mysql_execute.py
+++ app/tool/mysql_execute.py
@@ -4,15 +4,24 @@ from app.tool.base import BaseTool
 
 class MySQLExecuteTool(BaseTool):
     name: str = "query_mysql"
-    description: str = "查询本地 MySQL 数据库内容"
+    # 描述很重要，大模型根据描述判断什么时候调用工具
+    description: str = "该工具可以用来连接 MySQL 数据库，包括本地数据库和远程数据库"
 
     parameters: dict = {
         "type": "object",
         "properties": {
-            "host": {"type": "string", "description": "数据库地址"},
-            "user": {"type": "string", "description": "用户名"},
-            "password": {"type": "string", "description": "密码"},
-            "database": {"type": "string", "description": "数据库名"},
+            "host": {"type": "string",
+                     "description": "数据库地址",
+                     "default": "localhost"},
+            "user": {"type": "string",
+                     "description": "用户名",
+                     "default": "root"},
+            "password": {"type": "string",
+                         "description": "密码",
+                         "default": "mysql"},
+            "database": {"type": "string",
+                         "description": "数据库名",
+                         "default": "openmanus"},
             "query": {"type": "string", "description": "SQL 查询语句"}
         },
         "required": ["host", "user", "password", "database", "query"]
