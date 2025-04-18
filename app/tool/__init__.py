7206b8efc64c01957dcb0dcecaf2e864650d18b4
sleep-sleep-yyy <1051095699@qq.com>
2025-04-18 15:11:36 +0800

    Update __init__.py

--- app/tool/__init__.py
+++ app/tool/__init__.py
@@ -3,6 +3,7 @@ from app.tool.bash import Bash
 from app.tool.browser_use_tool import BrowserUseTool
 from app.tool.create_chat_completion import CreateChatCompletion
 from app.tool.deep_research import DeepResearch
+from app.tool.mysql_execute import MySQLExecuteTool
 from app.tool.planning import PlanningTool
 from app.tool.str_replace_editor import StrReplaceEditor
 from app.tool.terminate import Terminate
